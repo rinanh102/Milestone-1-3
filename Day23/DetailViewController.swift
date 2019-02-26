39f500a3e9cbf765ffa360766c5a648dfd6b93b3
Henry Nguyen <nvqdung@gmail.com>
2019-02-25 21:38:03 -0500

    Completed

---
+++ Day23/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  Day23
+//
+//  Created by henry on 25/02/2019.
+//  Copyright © 2019 HenryNguyen. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var selectedCountries : String?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let flagImage = selectedCountries{
+            imageView.image = UIImage(named: flagImage)
+        }
+        
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        imageView.layer.borderWidth = 1
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    }
+    @objc func shareTapped(){
+        guard let  image = imageView.image?.jpegData(compressionQuality: 0.9) else {
+            print("No image")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+}
--- Day23/ViewController.swift
+++ Day23/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var flagArray = [String]()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.rowHeight = 60
+    
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("3x.png"){
+                flagArray.append(item)
+            }
+        }
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flagArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "flagCell", for: indexPath)
+        if let str = flagArray[indexPath.row] as? String{
+            cell.textLabel?.text = str.dropLast(7).uppercased()
+        }
+//        cell.textLabel?.text = flagArray[indexPath.row]
+        cell.imageView?.image = UIImage(named: flagArray[indexPath.row])
+        
+        cell.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        cell.imageView?.layer.borderWidth = 1
+    
+        
+        return cell
     }
 
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "FlagDetail") as? DetailViewController{
+        
+        vc.selectedCountries = flagArray[indexPath.row]
+        if let str = flagArray[indexPath.row] as? String{
+            vc.title = str.dropLast(7).uppercased()
+            }
+        navigationController?.pushViewController(vc, animated: true)
+        }
+        
+    }
+    
 }
 
